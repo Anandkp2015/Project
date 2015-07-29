e350439716d0d47ec8da81f42ce0a7120d18c045
pranvwhiz <pranav@whiz-solutions.com>
2015-07-29 11:23:04 +0530

    I've changed that file Pranav

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ require_once('header.php');
 ?>
 <h1>Login form:</h1>
 <form name="admin login" class="form-horizontal" method="post">
-<label class="control-label">NAME</label>
+<label class="control-label">Pranav</label>
 <div class="controls">
 <input type="text" name="admin[name]" value="" class="span4 typeahead"></div><br/>
 <label class="control-label">PASSWORD</label>
